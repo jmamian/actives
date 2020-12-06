7b9dcaea8143458737fbd7e43967ccdc8d954efc
fabian borja <fabianborjaco@gmail.com>
2020-12-06 01:18:39 -0500

    Initial commit

---
+++ src/main/kotlin/com/uniajc/actives/ActivesApplication.kt
@@ -0,0 +1,11 @@
+package com.uniajc.actives
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ActivesApplication
+
+fun main(args: Array<String>) {
+	runApplication<ActivesApplication>(*args)
+}
---
+++ src/test/kotlin/com/uniajc/actives/ActivesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.uniajc.actives
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ActivesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
